03c5afd9f80053ba292b4a63dd8717a078a5a911
SudeSva <sssvs@LAPTOP-UA03QDD4>
2021-02-21 19:35:13 +0300

    20 ocak ders sonu

---
+++ GeneriscIntro/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneriscIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add (T item)
+        {
+            T[] tempArry = items; 
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < tempArry.Length; i++)
+            {
+                items[i] = tempArry[i];
+            }
+
+            items[items.Length - 1] = item;        }
+
+      
+    }
+}
---
+++ GeneriscIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneriscIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<String> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+
+        }
+    }
+}
