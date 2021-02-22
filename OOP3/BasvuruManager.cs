67c1fd2f91b2b8f3dd9120f2ade8894f00df824e
SudeSva <sssvs@LAPTOP-UA03QDD4>
2021-02-22 22:24:55 +0300

    23.01.2020

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        private IEnumerable<object> loggerServices;
+
+        //Method injection
+        public object KrediManager { get; private set; }
+
+        public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerService)
+        {
+            //Başvuru bilgilerini değerlendirme
+            //
+            krediManager.Hesapla();
+            foreach (var LoggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+        }      
+        
+         public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+         {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla(); 
+            }
+            
+
+            
+         }
+    
+    
+    
+    }
+}
---
+++ OOP3/DatabaseLoggerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class DatabaseLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Veritabanına loglandı");
+        }
+    }
+
+    class fileLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("dosyaya loglandı");
+        }
+    }
+
+
+
+
+
+
+
+}
+
+
