436bb698d87e1d45cd01cda28b6579b39f9ace68
Sude Savaş <77166988+sudesavas@users.noreply.github.com>
2021-03-04 17:06:18 +0300

    Update Program.cs

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -14,13 +14,11 @@ namespace DegerVeReferansTipler
             //sayi1 ??
 
             int[] sayilar1 = new int[] { 10, 20, 30 };
-            int[] sayilar2 = new int 
-
+            int[] sayilar2 = new int[] {100,200,300};
+            sayilar1=sayilar2;
+            sayilar[0]=999;
             
-            
-            
-            
-            
+            //sayilar1[0]? 999             
         }
     }
 }
